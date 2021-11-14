baf23f65d701e201982bf9b8b3b6f4e66ab66528
alike <ali.kellaway139@gmail.com>
2021-11-14 17:44:12 +0000

    Graph drawer now works

--- src/main/java/com/alike/graphical/TSPGraphDrawer.java
+++ src/main/java/com/alike/graphical/TSPGraphDrawer.java
@@ -1,18 +1,30 @@
 package com.alike.graphical;
 
 import com.alike.tspgraphsystem.TSPGraph;
-import javafx.scene.Scene;
+import javafx.animation.AnimationTimer;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 
-public class TSPGraphDrawer {
+public class TSPGraphDrawer extends AnimationTimer {
 
-    TSPGraph graph;
+    private Canvas canvas;
+    private TSPGraph graph;
+    private GraphicsContext graphicsContext;
+    private int editsPerRedraw;
 
-    public TSPGraphDrawer(TSPGraph graph, Scene scene) {
+    public TSPGraphDrawer(TSPGraph graph, Canvas canvas, int editsPerRedraw) {
         setGraph(graph);
+        setCanvas(canvas);
+        setGraphicsContext(canvas.getGraphicsContext2D());
+        setEditsPerRedraw(editsPerRedraw);
     }
 
-    public void draw() {
-
+    @Override
+    public void handle(long l) {
+        if (graph.getEdgeContainer().getEditCount() % editsPerRedraw == 0) {
+            clearCanvas(graphicsContext);
+            graph.drawGraph(graphicsContext);
+        }
     }
 
 
@@ -22,5 +34,36 @@ public class TSPGraphDrawer {
 
     public void setGraph(TSPGraph graph) {
         this.graph = graph;
+    }
+
+    public GraphicsContext getGraphicsContext() {
+        return graphicsContext;
+    }
+
+    public void setGraphicsContext(GraphicsContext graphicsContext) {
+        this.graphicsContext = graphicsContext;
+    }
+
+    /**
+     * Clears a canvas.
+     */
+    private void clearCanvas(GraphicsContext gc) {
+        gc.clearRect(0.0, 0.0, canvas.getWidth(), canvas.getHeight());
+    }
+
+    public Canvas getCanvas() {
+        return canvas;
+    }
+
+    public void setCanvas(Canvas canvas) {
+        this.canvas = canvas;
+    }
+
+    public int getEditsPerRedraw() {
+        return editsPerRedraw;
+    }
+
+    public void setEditsPerRedraw(int editsPerRedraw) {
+        this.editsPerRedraw = editsPerRedraw;
     }
 }
