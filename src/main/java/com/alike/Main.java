e3de9378b254615e25f40c34bbc1b9f1fc9082d0
alike <ali.kellaway139@gmail.com>
2021-11-14 18:39:53 +0000

    house keeping

--- src/main/java/com/alike/Main.java
+++ src/main/java/com/alike/Main.java
@@ -2,7 +2,7 @@ package com.alike;
 
 import com.alike.customexceptions.EdgeSuperimpositionException;
 import com.alike.customexceptions.NodeSuperimpositionException;
-import com.alike.graphical.TSPGraphDrawer;
+import com.alike.graphical.TSPGraphAnimator;
 import com.alike.tspgraphsystem.*;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
@@ -10,13 +10,14 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    public static final int WIDTH = 500;
-    public static final int HEIGHT = 500;
+    public static final int COORDINATE_MAX_WIDTH = 500;
+    public static final int COORDINATE_MAX_HEIGHT = 500;
+    public static final int WINDOW_MAX_WIDTH = (int) Math.ceil(COORDINATE_MAX_WIDTH + TSPGraphAnimator.NODE_DIAMETER);
+    public static final int WINDOW_MAX_HEIGHT =(int) Math.ceil(COORDINATE_MAX_HEIGHT + TSPGraphAnimator.NODE_DIAMETER);
     private final String STAGE_TITLE = "TSP";
     private Canvas canvas;
 
@@ -29,41 +30,30 @@ public class Main extends Application {
     public void start(Stage stage) {
         stage.setTitle(STAGE_TITLE);
         Group root = new Group();
-        canvas = new Canvas(WIDTH, HEIGHT);
-        TSPGraph graph = generateRandomGraph();
+        canvas = new Canvas(WINDOW_MAX_WIDTH, WINDOW_MAX_HEIGHT);
         root.getChildren().add(canvas);
-        Scene scene = new Scene(root, WIDTH, HEIGHT);
+        Scene scene = new Scene(root, WINDOW_MAX_WIDTH, WINDOW_MAX_HEIGHT);
         stage.setScene(scene);
         stage.show();
 
-
-        TSPGraphDrawer drawer = new TSPGraphDrawer(graph, canvas, 1);
+        AnimationTimer drawer = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                clearCanvas(canvas.getGraphicsContext2D());
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                TSPGraphAnimator.drawRandomGraph(canvas.getGraphicsContext2D());
+            }
+        };
+//        TSPGraphAnimator drawer = new TSPGraphAnimator(graph, canvas, 1);
         drawer.start();
     }
 
-    public TSPGraph generateRandomGraph() {
-        // Create our graph to draw
-        TSPNodeContainer nSet = new TSPNodeContainer();
-        int numNodes = 5;
-        for (int i = 0; i < numNodes; i++) {
-            try {
-                nSet.add(TSPNode.generateRandomTSPNode());
-            } catch (NodeSuperimpositionException e) {
-                i--;
-            }
-        }
-        TSPEdgeContainer eSet = new TSPEdgeContainer();
 
-        for (int x = 0; x < numNodes; x++) {
-            try {
-                eSet.add(new TSPEdge(nSet.getNodeSet().get(x), nSet.getNodeSet().get((x + 1) % numNodes)));
-            } catch (EdgeSuperimpositionException e) {
-                x--;
-            }
-        }
-        TSPGraph g = new TSPGraph();
-        g.setNodeContainer(nSet);
-        g.setEdgeContainer(eSet);
-        return g;
+    private void clearCanvas(GraphicsContext gc) {
+        gc.clearRect(0.0, 0.0, canvas.getWidth(), canvas.getHeight());
     }
 }
