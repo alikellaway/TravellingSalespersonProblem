7805eac0d45c8213696b02bc3abb0983f1d98968
alike <ali.kellaway139@gmail.com>
2021-11-23 17:48:11 +0000

    Javadocced the BruteForceSolver class.

--- src/main/java/com/alike/solutions/BruteForceSolver.java
+++ src/main/java/com/alike/solutions/BruteForceSolver.java
@@ -9,19 +9,59 @@ import com.alike.tspgraphsystem.*;
 import javafx.util.Pair;
 import java.util.List;
 
+/**
+ * Class contains logic to solve TSPGraphs containing node sets using the brute force method by finding the permutations
+ * of node orders and traversing them all to find the shortest route.
+ * @author alike
+ */
 public class BruteForceSolver {
 
-    TSPGraph graph;
-    Permuter<Integer> permuter;
-    double shortestFoundRoute = Integer.MAX_VALUE;
-    List<Integer> shortestFoundPerm;
+    /**
+     * The graph the solution will be running on.
+     */
+    private TSPGraph graph;
 
+    /**
+     * The @code{Permuter} object that will handle the permutation calculations of nodes for the solution to run.
+     */
+    private Permuter<Integer> permuter;
+
+    /**
+     * The current shortest found route during the solution run (initialised to max value).
+     */
+    private double shortestFoundRoute = Integer.MAX_VALUE;
+
+    /**
+     * The permutation which led to the shortest found route so far.
+     */
+    private List<Integer> shortestFoundPerm;
+
+    /**
+     * Used to initialise a new @code{BruteForceSolver} object.
+     * @param graph The TSPGraph that this solution will run on.
+     */
     public BruteForceSolver(TSPGraph graph) {
         setGraph(graph);
         setPermuter(new Permuter<>(graph.getNodeContainer().getNodeIDs()));
     }
 
-    public Pair<TSPGraph, Double> runSolution(int delayPerStep) throws PermutationExhaustionException, EdgeSuperimpositionException, PermutationFocusException, NonExistentNodeException, InterruptedException {
+    /**
+     * Starts the solution running on the @code{graph}
+     * @param delayPerStep The time delay between checking different permutations (to slow the progress if required).
+     * @return Returns a pair object containing the TSPGraph (which contains the solution edge set) and its route
+     * length.
+     * @throws PermutationExhaustionException Thrown if @code{Permuter} object is bugged or incorrectly used.
+     * @throws EdgeSuperimpositionException Thrown if an edge is imposed onto another.
+     * @throws PermutationFocusException Thrown if @code{Permuter} object is bugged or incorrectly used.
+     * @throws NonExistentNodeException Thrown if the solution runs into a node ID that doesn't exist.
+     * @throws InterruptedException Thrown if the thread is interrupted.
+     */
+    public Pair<TSPGraph, Double> runSolution(int delayPerStep) throws PermutationExhaustionException,
+                                                                        EdgeSuperimpositionException,
+                                                                            PermutationFocusException,
+                                                                                NonExistentNodeException,
+                                                                                    InterruptedException {
+        // While there are still permutations we haven't checked we wish to continue checking more.
         while (permuter.hasUnseenPermutations()) {
             // Set the graphs edges to be a new edge container containing the edges constructed from a permutation
             graph.setEdgeContainer(createEdgeContainerFromNodeSetPermutation(permuter.getNextPermutation()));
@@ -39,28 +79,43 @@ public class BruteForceSolver {
         return new Pair<>(graph, shortestFoundRoute);
     }
 
-    private void setGraph(TSPGraph graph) {
-        this.graph = graph;
-    }
-
-    private void setPermuter(Permuter<Integer> permuter) {
-        this.permuter = permuter;
-    }
-
-    private TSPEdgeContainer createEdgeContainerFromNodeSetPermutation(List<Integer> nodeOrder)
+    /**
+     * Outputs a new @code{edgeContainer} object given a list of node IDs.
+     * @param nodeIDs A permutation of the node IDs to give a new order.
+     * @return @code{edgeContainer} A new @code{edgeContainer} object containing edges abiding by the node order specified by
+     * the nodeIDs parameter.
+     * @throws EdgeSuperimpositionException Thrown if two edges are added between the same nodes.
+     * @throws NonExistentNodeException Thrown if a node ID is encountered that does not exist.
+     */
+    private TSPEdgeContainer createEdgeContainerFromNodeSetPermutation(List<Integer> nodeIDs)
             throws EdgeSuperimpositionException, NonExistentNodeException {
         TSPEdgeContainer edgeContainer = new TSPEdgeContainer();
         TSPNodeContainer nodeContainer = graph.getNodeContainer();
         int numNodes = nodeContainer.getNodeSet().size();
 
-        for (int idx = 0; idx < nodeOrder.size(); idx++) {
-            TSPNode startNode = nodeContainer.getNodeByID(nodeOrder.get(idx));
+        for (int idx = 0; idx < nodeIDs.size(); idx++) {
+            TSPNode startNode = nodeContainer.getNodeByID(nodeIDs.get(idx));
             int idxOfEndNode = (idx + 1) % numNodes;
-            TSPNode endNode = nodeContainer.getNodeByID(nodeOrder.get(idxOfEndNode));
+            TSPNode endNode = nodeContainer.getNodeByID(nodeIDs.get(idxOfEndNode));
             TSPEdge newEdge = new TSPEdge(startNode, endNode);
             edgeContainer.add(newEdge);
         }
         return edgeContainer;
     }
 
+    /**
+     * Sets the value @code{graph} attribute.
+     * @param graph The new value to assign the @code{graph} attribute.
+     */
+    private void setGraph(TSPGraph graph) {
+        this.graph = graph;
+    }
+
+    /**
+     * Sets the @code{permuter} attribute to a new @code{Permuter} object.
+     * @param permuter The new @code{Permuter} to become the @code{permuter} attribute.
+     */
+    private void setPermuter(Permuter<Integer> permuter) {
+        this.permuter = permuter;
+    }
 }
