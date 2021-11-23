aac3ee53635481a50e5e40f715872378e837dbdb
alike <ali.kellaway139@gmail.com>
2021-11-23 17:48:25 +0000

    Javadocced the PermutationFocusException.java class.

--- src/main/java/com/alike/customexceptions/PermutationFocusException.java
+++ src/main/java/com/alike/customexceptions/PermutationFocusException.java
@@ -1,5 +1,8 @@
 package com.alike.customexceptions;
 
+/**
+ * Thrown by a @code{Permuter} object if its @code{focusIdx} attribute encounters list index errors.
+ */
 public class PermutationFocusException extends Exception {
     public PermutationFocusException(String msg) {
         super(msg);
