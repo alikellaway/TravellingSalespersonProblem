cb9d4582b520a6dfcab3937d5adf3d8192751e9c
alike <ali.kellaway139@gmail.com>
2021-11-23 17:20:59 +0000

    Up to date main classes

--- src/main/java/com/alike/Main.java
+++ src/main/java/com/alike/Main.java
@@ -1,26 +1,26 @@
 package com.alike;
 
-import com.alike.customexceptions.EdgeSuperimpositionException;
-import com.alike.customexceptions.InvalidGraphException;
+import com.alike.customexceptions.*;
 import com.alike.graphical.TSPGraphAnimator;
-import com.alike.solutions.NearestNeighbourSolver;
+import com.alike.solutions.BruteForceSolver;
 import com.alike.tspgraphsystem.TSPGraph;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
+import javafx.util.Pair;
 
 public class Main extends Application {
     /**
      * The maximum value x value that coordinates are allowed to be given.
      */
-    public static final int COORDINATE_MAX_WIDTH = 1800;
+    public static final int COORDINATE_MAX_WIDTH = 400;
 
     /**
      * The maximum value y value that coordinates are allowed to be given.
      */
-    public static final int COORDINATE_MAX_HEIGHT = 900;
+    public static final int COORDINATE_MAX_HEIGHT = 400;
 
     /**
      * The maximum width value the window and canvas can be given.
@@ -37,19 +37,35 @@ public class Main extends Application {
      */
     private final String STAGE_TITLE = "TSP";
 
-    private static TSPGraph graph = TSPGraph.generateRandomGraph(10000, false);
+    private static TSPGraph nnsGraph = TSPGraph.generateRandomGraph(100000, false);
+    private static TSPGraph bsGraph = TSPGraph.generateRandomGraph(4, false);
 
 
     public static void main(String[] args) {
-        Thread t = new Thread(() -> {
-            try {
-                NearestNeighbourSolver nns = new NearestNeighbourSolver(graph);
-                nns.runSolution(5);
-            } catch (InvalidGraphException | InterruptedException | EdgeSuperimpositionException e) {
-                e.printStackTrace();
-            }
+
+        // Nearest neighbour solver
+//        Thread nnsT = new Thread(() -> {
+//            try {
+//                NearestNeighbourSolver nns = new NearestNeighbourSolver(nnsGraph);
+//                nns.runSolution(0);
+//            } catch (InvalidGraphException | InterruptedException | EdgeSuperimpositionException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//        nnsT.start();
+        // Brute force solver
+        Thread bsT = new Thread(() -> {
+           try {
+               BruteForceSolver bs = new BruteForceSolver(bsGraph);
+               Pair<TSPGraph, Double> solutionOutput = bs.runSolution(1);
+               System.out.println(solutionOutput.getKey());
+               System.out.println(solutionOutput.getValue());
+
+           } catch (PermutationExhaustionException | EdgeSuperimpositionException | PermutationFocusException | NonExistentNodeException | InterruptedException e) {
+               e.printStackTrace();
+           }
         });
-        t.start();
+        bsT.start();
         launch(args);
     }
 
@@ -64,7 +80,7 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
 
-        TSPGraphAnimator drawer = new TSPGraphAnimator(graph, canvas, 1);
+        TSPGraphAnimator drawer = new TSPGraphAnimator(scene, canvas, bsGraph,1);
         drawer.start();
     }
 }
--- src/main/java/com/alike/Main2.java
+++ src/main/java/com/alike/Main2.java
@@ -2,21 +2,34 @@ package com.alike;
 
 
 
+import com.alike.customexceptions.PermutationExhaustionException;
+import com.alike.customexceptions.PermutationFocusException;
+
 import java.util.ArrayList;
 
 
 public class Main2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PermutationExhaustionException {
         ArrayList<Integer> a = new ArrayList<>();
         a.add(1);
         a.add(2);
         a.add(3);
-        a.add(4);
 
 
 
-        Permuter p1 = new Permuter(a);
-//        System.out.println(p1.getNextPermutation());
+
+        Permuter<Integer> p1 = new Permuter<Integer>(a);
+        try {
+//        System.out.println(p1.getCurrentPermutation());
+//        System.out.println(p1.getCurrentPermutation());
+            System.out.println(p1.getNextPermutation());
+            System.out.println(p1.getCurrentPermutation());
+            System.out.println(p1.getNextPermutation());
+            System.out.println(p1.getCurrentPermutation());
+        } catch (PermutationFocusException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
