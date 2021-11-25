1f2068f31403d9e3dfbec640f61c5c5fa24fbfe1
alike <ali.kellaway139@gmail.com>
2021-11-25 13:25:26 +0000

    Added new exception to help with hilbert animator and solver

---
+++ src/main/java/com/alike/customexceptions/NonSquareCanvasException.java
@@ -0,0 +1,11 @@
+package com.alike.customexceptions;
+
+/**
+ * Thrown when a canvas input into a @code{HilbertFractalCurveAnimator} or @code{HilbertFractalCurveSolve} is not a
+ * perfect square.
+ */
+public class NonSquareCanvasException extends Exception {
+    public NonSquareCanvasException(String msg) {
+        super(msg);
+    }
+}
