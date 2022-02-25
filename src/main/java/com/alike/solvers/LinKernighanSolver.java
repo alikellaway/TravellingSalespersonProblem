3b24aaaeb21e1e30bc746b252913df978d780378
alike <ali.kellaway139@gmail.com>
2022-02-25 13:43:30 +0000

    Added the LK solver class and commented out some of the prior unfunctional christofides work.

---
+++ src/main/java/com/alike/solvers/LinKernighanSolver.java
@@ -0,0 +1,16 @@
+package com.alike.solvers;
+
+import com.alike.solvertestsuite.Solution;
+import com.alike.tspgraphsystem.TSPGraph;
+
+public class LinKernighanSolver implements Solver {
+    @Override
+    public Solution runSolution(int delayPerStep) {
+        return null;
+    }
+
+    @Override
+    public void setGraph(TSPGraph graph) {
+
+    }
+}
