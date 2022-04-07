27872da42653aef67ff2f1e5419027b3d7be707a
alike <ali.kellaway139@gmail.com>
2022-04-07 12:30:15 +0100

    Removing some redundant code from NNS and tidying

--- src/main/java/com/alike/solvers/NearestNeighbourSolver.java
+++ src/main/java/com/alike/solvers/NearestNeighbourSolver.java
@@ -1,6 +1,5 @@
 package com.alike.solvers;
 
-import com.alike.Main;
 import com.alike.customexceptions.EdgeSuperimpositionException;
 import com.alike.customexceptions.EdgeToSelfException;
 import com.alike.customexceptions.NoClosestNodeException;
@@ -9,7 +8,6 @@ import com.alike.solvertestsuite.Fail;
 import com.alike.solvertestsuite.Solution;
 import com.alike.solvertestsuite.SolverOutput;
 import com.alike.staticgraphsystem.*;
-import java.util.ArrayList;
 
 /**
  * This class takes a graph object as a parameter, and will create a TSP route using the 'Nearest Neighbour Algorithm'.
@@ -21,20 +19,14 @@ public class NearestNeighbourSolver implements StaticSolver {
      * A reference to the graph we are solving.
      */
     private StaticGraph graph;
+
     /**
-     * A reference to the edge container we are operating within.
-     */
-    private EdgeContainer edgeContainer;
-    /**
-     * A reference to the node container we are operating on.
-     */
-    private NodeContainer nodeContainer;
-    /**
-     * The node the algorithm is currently sitting on.
+     * The node the algorithm is currently focussed on.
      */
     private Node currentNode;
+
     /**
-     * This records the number of nodes we have already visited. We do this so we don't have to repeatedly loop
+     * This records the number of nodes we have already visited. We do this, so we don't have to repeatedly loop
      * through the list to check they've all been visited.
      */
     private int numNodesVisited;
@@ -51,8 +43,7 @@ public class NearestNeighbourSolver implements StaticSolver {
     /**
      * Constructs a new empty @code{NearestNeighbourSolver} object.
      */
-    public NearestNeighbourSolver() {
-    }
+    public NearestNeighbourSolver() {}
 
     /**
      * Method starts the algorithm that solves the TSP through the graph.
@@ -65,11 +56,11 @@ public class NearestNeighbourSolver implements StaticSolver {
             graph.getEdgeContainer().clear(); // Make sure edge set is empty
             long startTime = System.nanoTime();
             // Set our current node to be the first node in the list of nodes (could choose any).
-            setCurrentNode(nodeContainer.getNodeSet().get(0)); // Current node is 0th
+            setCurrentNode(graph.getNodeContainer().getNodeSet().get(0)); // Current node is 0th
             currentNode.setVisited(true); // Set it as visited
             numNodesVisited = 1;
             // Execute the traversal steps
-            while (numNodesVisited <= nodeContainer.getNodeSet().size()) {
+            while (numNodesVisited <= graph.getNodeContainer().getNodeSet().size()) {
                 try {
                     traverseToNextClosestNode();
                     RepeatedFunctions.sleep(delayPerStep);
@@ -102,12 +93,12 @@ public class NearestNeighbourSolver implements StaticSolver {
         // Find the next closest unvisited node.
         Node nextNode;
         try {
-            nextNode = currentNode.getClosestNode(nodeContainer.getNodeSet(), true);
+            nextNode = currentNode.getClosestNode(graph.getNodeContainer().getNodeSet(), true);
         } catch (NoClosestNodeException e) { // If we didn't find one, we have exhausted nodes, loop back to beginning
-            nextNode = nodeContainer.getNodeSet().get(0);
+            nextNode = graph.getNodeContainer().getNodeSet().get(0);
         }
         // Add an edge between the current node and the next closest node.
-        edgeContainer.add(new Edge(currentNode, nextNode));
+        graph.getEdgeContainer().add(new Edge(currentNode, nextNode));
         nextNode.setVisited(true); // Set that node to visited.
         // System.out.println(nextNode.isVisited());
         setCurrentNode(nextNode); // Change the current node.
@@ -121,7 +112,5 @@ public class NearestNeighbourSolver implements StaticSolver {
     public void setGraph(StaticGraph newGraph) {
         RepeatedFunctions.validateGraph(newGraph);
         this.graph = newGraph;
-        this.nodeContainer = newGraph.getNodeContainer();
-        this.edgeContainer = newGraph.getEdgeContainer();
     }
 }
