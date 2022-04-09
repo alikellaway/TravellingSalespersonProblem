eb65778a108aa53bf78cea9ac480306a3ad6485b
alike <ali.kellaway139@gmail.com>
2022-04-09 21:17:42 +0100

    Finished the graph writer class. Can now store graphs on mass.

---
+++ src/main/java/com/alike/read_write/GraphWriter.java
@@ -0,0 +1,66 @@
+package com.alike.read_write;
+
+import com.alike.graphsystem.Graph;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Writes graphs in storage format into text files at the path FILE_PATH.
+ * @author alike
+ */
+public class GraphWriter {
+    /**
+     * The location at which graphs will be written.
+     */
+    private static final String FILE_PATH = "graphs.txt";
+
+    /**
+     * The @code{BufferedWriter} object used to write into the file.
+     */
+    private final BufferedWriter bw;
+
+    /**
+     * Creates a new GraphWriter object.
+     * @throws IOException Thrown if the file does not exist.
+     */
+    public GraphWriter() throws IOException {
+        bw = new BufferedWriter(new FileWriter(FILE_PATH, true));
+    }
+
+    /**
+     * Used to append a new coordintae list to the bottom of the coordinate list file.
+     * @param graph The graph to be written.
+     * @throws IOException Thrown if an I/O error is thrown.
+     */
+    public void writeGraph(Graph graph) throws IOException {
+        bw.write(graph.toStorageFormat(';') + "\n");
+    }
+
+    /**
+     * Use to remove all information from the file at file path.
+     * @throws IOException Thrown if an io error is thrown.
+     */
+    public void clearFile() throws IOException {
+        BufferedWriter bw2 = new BufferedWriter(new FileWriter(FILE_PATH, false));
+        bw2.write("");
+        bw2.close();
+    }
+
+    /**
+     * Closes the buffered writer.
+     * @throws IOException Thrown if the bw fails to close.
+     */
+    public void close() throws IOException {
+        bw.close();
+    }
+
+    /**
+     * Flushes the @code{bw}'s stream.
+     * @throws IOException Throws if an I/O error occurs.
+     */
+    public void flush() throws IOException {
+        bw.flush();
+    }
+}
