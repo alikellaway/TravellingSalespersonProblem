ed9c7e7039b3043696e2ffaaec957cf56fc578d5
alike <ali.kellaway139@gmail.com>
2022-04-09 14:11:43 +0100

    Added some functionality.

--- src/main/java/com/alike/solution_helpers/Timer.java
+++ src/main/java/com/alike/solution_helpers/Timer.java
@@ -19,13 +19,29 @@ public class Timer {
      * Begins a timer that times the given number of milliseconds before switching a Boolean.
      * @param timerLength The number of milliseconds which the timer should time during this run.
      */
-    public void time(int timerLength) {
+    private void time(int timerLength) {
         this.timing = true;
         Thread t = new Thread(() -> {
-            RepeatedFunctions.sleep(timerLength);
-            this.timing = false;
+            runTimer(timerLength);
         });
         t.start();
+    }
+
+    /**
+     * Runs the timer for a specified amount of milliseconds.
+     * @param timerLength The number of millisecons the timer should run for.
+     * @param inInternalThread If the timer should time in the thread it's constructed in or if it should time in a
+     *                         thread created inside this object (used for different things). When true, the thread in
+     *                         which the Timer object is created can continue running while the timer is timing; if
+     *                         false then the outcome will be making the thread the time() method is called in sleep().
+     */
+    public void time(int timerLength, boolean inInternalThread) {
+        this.timing = true;
+        if (inInternalThread) {
+            time(timerLength);
+        } else {
+            runTimer(timerLength);
+        }
     }
 
     /**
@@ -34,5 +50,14 @@ public class Timer {
      */
     public boolean isTiming() {
         return this.timing;
+    }
+
+    /**
+     * Used internally to make the thread its called in sleep for the input amount of time.
+     * @param milliseconds The number of milliseconds which the thread should sleep for.
+     */
+    private void runTimer(int milliseconds) {
+        RepeatedFunctions.sleep(milliseconds);
+        this.timing = false;
     }
 }
--- src/main/java/com/alike/solvertestsuite/Stopwatch.java
+++ src/main/java/com/alike/solvertestsuite/Stopwatch.java
@@ -1,5 +1,10 @@
 package com.alike.solvertestsuite;
 
+/**
+ * Class can be used to measure execution time of other code by calling start and stop methods, similar to how a
+ * stop watch works in real life.
+ * @author alike
+ */
 public class Stopwatch {
     /**
      * The value representing invalidity in this class.
@@ -17,9 +22,19 @@ public class Stopwatch {
     private long stopTime = invalid;
 
     /**
-     * Creates a new Stopwatch object.
+     * Initialises a new stop watch object.
      */
     public Stopwatch() {}
+
+    /**
+     * Creates a new stop watch that is starting if the input boolean is true.
+     * @param startNow Whether this stop watch should be started immediately.
+     */
+    public Stopwatch(boolean startNow) {
+        if (startNow) {
+            start();
+        }
+    }
 
     /**
      * Notes a start time for the stop watch.
@@ -34,31 +49,47 @@ public class Stopwatch {
     public void stop() {
         if (getStartTime() != invalid) {
             setStopTime(System.nanoTime());
+        } // Only set a stop time if we have a start time.
+    }
+
+    /**
+     * Stops the stopwatch if not already stopped and returns the time this stop watch was running for in nano-seconds.
+     * @return long The number of nano-seconds this stop watch was running for.
+     */
+    public long getTimeNs() {
+        if (stopTime != invalid){
+            return stopTime - startTime;
+        } else if (isStarted()) {
+            stop();
+            return getTimeNs();
+        } else {
+            return invalid;
         }
     }
 
     /**
-     * Returns the time this stop watch was running for in nano-seconds.
-     * @return long The number of nano-seconds this stop watch was running for.
-     */
-    public long getTimeNs() {
-        return stopTime - startTime;
-    }
-
-    /**
-     * Returns the time this stop watch was running for in micro-seconds.
+     * Stops the stopwatch if not already stopped and returns the time this stop watch was running for in
+     * micro-seconds.
      * @return micro-seconds The time elapsed in micro-seconds.
      */
     public long getTimeMus() {
-        return getTimeNs()/1000;
+        long val = getTimeNs();
+        if (val != invalid) {
+            return getTimeNs()/1000;
+        }
+        return val;
     }
 
     /**
-     * Returns the time this stop watch was running for in milliseconds.
+     * Stops the stopwatch if not already stopped and returns the time this stop watch was running for in milliseconds.
      * @return milliseconds The time elapsed in milliseconds.
      */
     public long getTimeMs() {
-        return getTimeMus()/1000;
+        long val = getTimeMus();
+        if (val != invalid) {
+            return getTimeMus()/1000;
+        }
+        return val;
     }
 
     /**
@@ -91,5 +122,21 @@ public class Stopwatch {
      */
     public void setStopTime(long stopTime) {
         this.stopTime = stopTime;
+    }
+
+    /**
+     * Used to reset the stopwatch (so it might be used again)
+     */
+    public void clear() {
+        setStartTime(invalid);
+        setStopTime(invalid);
+    }
+
+    /**
+     * Returns a boolean that is true if the @code{start()} method has been called.
+     * @return boolean true if the stop watch has been started.
+     */
+    public boolean isStarted() {
+        return startTime != invalid;
     }
 }
