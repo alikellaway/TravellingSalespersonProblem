bf76759852e557933a5fe400de1c0428d2773cd3
Ali Kellaway Laptop <ali.kellaway139@gmail.com>
2022-04-10 16:57:59 +0100

    Fleshed this out with javadocs and such.

--- src/main/java/com/alike/solvertestsuite/DynamicTestResult.java
+++ src/main/java/com/alike/solvertestsuite/DynamicTestResult.java
@@ -1,5 +1,9 @@
 package com.alike.solvertestsuite;
 
+/**
+ * Holds information output by a test of a dynamic solver.
+ * @author alike
+ */
 public class DynamicTestResult {
     /**
      * The number of solves the solver had to do in the test.
@@ -9,26 +13,64 @@ public class DynamicTestResult {
     /**
      * The total time taken to complete all the solves.
      */
-    private long totalTime;
+    private long totalTimeTaken;
+
+    /**
+     * The solution object output by a solver.
+     */
+    private DynamicSolution sol;
 
     public DynamicTestResult(DynamicSolution sol, int numSolves, long totalTime) {
         setNumSolves(numSolves);
-        setTotalTime(totalTime);
+        setTotalTimeTaken(totalTime);
+        setSol(sol);
     }
 
+    /**
+     * Returns the value of the @code{numSolves} attribute.
+     * @return numSolves The value of the @code{numSolves} attribute.
+     */
     public double getNumSolves() {
         return numSolves;
     }
 
+    /**
+     * Assigns the value of the @code{numSolves} attribute.
+     * @param numSolves The new value to assign the @code{numSolves} attribute.
+     */
     public void setNumSolves(int numSolves) {
         this.numSolves = numSolves;
     }
 
-    public long getTotalTime() {
-        return totalTime;
+    /**
+     * Returns the value of the @code{totalTimeTaken} attribute.
+     * @return totalTimeTaken The value of the @code{totalTimeTaken} attribute.
+     */
+    public long getTotalTimeTaken() {
+        return totalTimeTaken;
     }
 
-    public void setTotalTime(long totalTime) {
-        this.totalTime = totalTime;
+    /**
+     * Assigns the value of the @code{totalTimeTaken} attribute.
+     * @param totalTimeTaken The new value to assign the @code{totalTimeTaken} attribute.
+     */
+    public void setTotalTimeTaken(long totalTimeTaken) {
+        this.totalTimeTaken = totalTimeTaken;
+    }
+
+    /**
+     * Returns the value of the @code{sol} attribute.
+     * @return sol The value of the @code{sol} attribute.
+     */
+    public DynamicSolution getSol() {
+        return sol;
+    }
+
+    /**
+     * Assigns the value of the @code{sol} attribute.
+     * @param sol The new value to assign the @code{sol} attribute.
+     */
+    public void setSol(DynamicSolution sol) {
+        this.sol = sol;
     }
 }
