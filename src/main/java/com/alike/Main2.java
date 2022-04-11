182f8720b729a39a7a3568d5763898a93f055667
alike <ali.kellaway139@gmail.com>
2022-04-11 18:40:51 +0100

    Corrected the writing part to produce the correct number of tests.

--- src/main/java/com/alike/Main2.java
+++ src/main/java/com/alike/Main2.java
@@ -40,7 +40,7 @@ public class Main2 {
         // Create some new graphs and write them in.
         GraphWriter gw = new GraphWriter();
         gw.clearFile();
-        for (int i = 3; i < 103; i++) {
+        for (int i = 3; i <= 103; i++) {
             DynamicGraph g = GraphGenerator.generateRandomDynamicGraph(i, 10, false, false);
             gw.writeGraph(g);
         }
