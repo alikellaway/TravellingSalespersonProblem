2802739f59e2842122598ef79df2714e0fcbf487
alike <ali.kellaway139@gmail.com>
2022-04-20 21:07:33 +0100

    Updated a todo comment.

--- src/main/java/com/alike/graphsystem/CoordinateMover.java
+++ src/main/java/com/alike/graphsystem/CoordinateMover.java
@@ -81,7 +81,9 @@ public class CoordinateMover {
      * @param v The vector by which to move the coordinate.
      */
     private void moveCoordinateByAVector(Coordinate c, Vector v) {
-        // TODO Must be a way to make this not move right angles: loop?
+        /* TODO Must be a way to make this not move right angles: loop? It starts to look weird when high speeds are
+            used.
+        */
         // Do the x component of the vector first.
         if (v.getX() < 0) { // The vector is moving the coordinate left.
             for (int x = 0; x < v.getX() * -1; x++) {
